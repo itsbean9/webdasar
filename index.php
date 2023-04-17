866ebb1cebbdba4d52ffad969fc0c0702087bc87
itsbean9 <windiwidyaningsih09@gmail.com>
2023-04-17 18:39:02 +0700

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    $nama_kamu = "Windi";
+    $umur = 21;
+    // tanda petik dua
+    /*
+    echo "Hello World, Welcome ".$nama_kamu;
+    echo "Umur kamu ".$umur;
+    echo "tahun";
+    echo "<br/>";
+    */
+    echo "Hello World, Welcome ".$nama_kamu." Umur Kamu ".$umur." tahun";
+    echo "<br/>";
+    echo "Berarti $nama_kamu lahir di tahun ";
+    echo "2023-$umur;
+    // tanda petik satu
+    // echo 'Hello World';
+?>
