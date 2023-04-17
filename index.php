1bfa87eb37da046d9bf2cf6018c288a2879e01fa
itsbean9 <windiwidyaningsih09@gmail.com>
2023-04-17 18:55:24 +0700

    modifikasi index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <?php
+
     $nama_kamu = "Windi";
-    $umur = 21;
+    $umur = 22;
+
     // tanda petik dua
     /*
     echo "Hello World, Welcome ".$nama_kamu;
-    echo "Umur kamu ".$umur;
-    echo "tahun";
+    echo " Umur kamu ".$umur;
+    echo " tahun";
     echo "<br/>";
     */
     echo "Hello World, Welcome ".$nama_kamu." Umur Kamu ".$umur." tahun";
     echo "<br/>";
     echo "Berarti $nama_kamu lahir di tahun ";
-    echo "2023-$umur;
+    echo 2023-$umur;
     // tanda petik satu
     // echo 'Hello World';
 ?>
