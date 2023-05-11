32cd9cc10536167fd6c0f82daa8371f5fa5ee10d
itsbean9 <windiwidyaningsih09@gmail.com>
2023-05-11 09:09:48 +0700

    project CRUD

---
+++ Public/conn.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$base = "mahasiswa";
+
+$conn = mysqli_connect($host, $user, $pass, $base);
+if (mysqli_connect_errno()) {
+    die("failed to connect");
+}
